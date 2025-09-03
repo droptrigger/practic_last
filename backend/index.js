8bbec54963d4b740993bda0d36673ab0fb3eafa8
vasil1x <91758220+droptrigger@users.noreply.github.com>
2025-09-03 13:43:45 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
